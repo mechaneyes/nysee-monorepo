d097cef96d897834f980f471fccb6e6fa595c5e1
mechaneyes <ray@mechaneyes.com>
2024-08-14 17:14:28 -0400

    revert to process.env.WORDPRESS_API_URL

--- apps/optic/next.config.js
+++ apps/optic/next.config.js
@@ -1,7 +1,7 @@
-if (!URL.canParse(process.env.NEXT_PUBLIC_WORDPRESS_API_URL)) {
+if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
-    Add to your environment variables NEXT_PUBLIC_WORDPRESS_API_URL.
+    Add to your environment variables WORDPRESS_API_URL.
   `);
 }
 
