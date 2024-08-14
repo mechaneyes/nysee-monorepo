0440b9398459918b2e348d588e49c5298bd31ecf
mechaneyes <ray@mechaneyes.com>
2024-08-14 17:22:33 -0400

    just removed it from next.config.js entirely 🤞
    ° please!

--- apps/optic/next.config.js
+++ apps/optic/next.config.js
@@ -1,19 +1,3 @@
-if (!process.env.WORDPRESS_API_URL) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
-  `);
-}
-
-try {
-  new URL(process.env.WORDPRESS_API_URL);
-} catch (err) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    The provided URL is invalid: ${process.env.WORDPRESS_API_URL}
-  `);
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
