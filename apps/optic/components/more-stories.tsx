1b89bc307454d3af787f176ce0e8857475149d64
mechaneyes <ray@mechaneyes.com>
2024-08-22 09:49:53 -0400

    changed grid config for <MoreStories />
    ° 2 across was just too big
    ° breakpoints to show more in a row at various viewports
    ° md:grid-cols-2 lg:grid-cols-3
    ° md:gap-x-8

--- apps/optic/components/more-stories.tsx
+++ apps/optic/components/more-stories.tsx
@@ -6,7 +6,7 @@ export default function MoreStories({ posts }) {
       <h2 className="px-5">
         More Down Low
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 gap-y-20 mb-32 px-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-8 lg:grid-cols-3 gap-y-20 mb-32 px-5">
         {posts.map(({ node }) => (
           <PostPreview
             key={node.slug}
