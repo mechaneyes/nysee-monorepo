6699e093f5b1ea32b1325dd3b908428bf607e344
mechaneyes <ray@mechaneyes.com>
2024-08-23 02:09:25 -0400

    /home tailwind ° layout+page.js to (pages) ° homepage styling

--- apps/home/app/(pages)/page.js
+++ apps/home/app/(pages)/page.js
@@ -3,12 +3,12 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import RadarVideo from "./components/radar-video";
-import RadarSequence from "./components/radar-sequence";
+import RadarVideo from "../components/radar-video";
+import RadarSequence from "../components/radar-sequence";
 
 export default function Home() {
   const [radarElement, setRadarElement] = useState(null);
-  
+
   useEffect(() => {
     const isIOS =
       /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
@@ -25,17 +25,21 @@ export default function Home() {
     <div className="homepage">
       <div className="homepage__header">
         <h1 style={{ padding: "1rem 0 0", color: "black" }}>NYSee.nyc</h1>
-        <div className="homepage__header__subheader">
-          <Link href="/ray-weitzenberg">
-            <h3>Ray Weitzenberg</h3>
-            <h4>Let&apos;s Dance: @mechaneyes</h4>
+        <div className="mb-4">
+          <Link href="/ray-weitzenberg" className="grid grid-rows-2 gap-0">
+            <h3 className="text-2xl">Ray Weitzenberg</h3>
+            <h4 className="text-xl">Let&apos;s dance: @mechaneyes</h4>
           </Link>
         </div>
-        <div className="homepage__header__subheader homepage__header__subheader--radar">
-          <Link href="/lowdown">
-            <h3>NYSee Lowdown</h3>
-            <h4>Beyond the Frame</h4>
+        <div className="mb-4">
+          <Link href="/lowdown" className="grid grid-rows-2 gap-0">
+            <h3 className="text-2xl">NYSee Lowdown</h3>
+            <h4 className="text-xl">Beyond the frame</h4>
           </Link>
+        </div>
+        <div className="grid grid-rows-2 gap-0">
+          <h3 className="text-2xl">NYSee Radar</h3>
+          <h4 className="text-xl">Art pulse, curated</h4>
         </div>
       </div>
       <Image
