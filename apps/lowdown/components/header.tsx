76aecd2515c2886442690822fe7faf5e1262b4c9
mechaneyes <ray@mechaneyes.com>
2024-08-26 11:35:53 -0400

    🔻 edits to masthead and header
    ° header <ul> same as masthead
    ° still using <h2> for header, but centering it w nav elements on line below

--- apps/lowdown/components/header.tsx
+++ apps/lowdown/components/header.tsx
@@ -4,24 +4,14 @@ import NavMenu from "@/components/ui/nav-menu";
 export default function Header() {
   return (
     <>
-      <header className="flex items-center justify-between px-5 md:px-8 py-4 border-b">
-        <div className="flex items-center space-x-4">
+      <header className="masthead flex flex-col items-center justify-between px-5 md:px-8 py-4 border-b">
+        <div className="flex items-center space-x-4 mb-3">
           <h2 className="">
-            <Link href="/" className="hover:underline">
+            <Link href="/">
               NYSee Lowdown
             </Link>
           </h2>
         </div>
-        {/* <nav className="flex items-center space-x-3">
-          <a href="/" className="text-gray-700">
-            NYSee Home
-          </a>
-          <div>&middot;</div>
-          <a href="https://nysee.nyc/ray-weitzenberg" className="text-gray-700">
-            Ray Weitzenberg
-          </a>
-        </nav> */}
-
         <NavMenu />
       </header>
     </>
