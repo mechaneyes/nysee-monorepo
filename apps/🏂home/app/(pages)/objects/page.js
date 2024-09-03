e36838e6b668f56c94a1a5e0a63b7774a7e1af36
mechaneyes <ray@mechaneyes.com>
2024-09-03 19:45:31 -0400

    objects + projects pages
    ° restyled part of layout. had broken somehow
    ° added metadata incl og image
    ° link hover state mechanics in typography.scss

--- apps/🏂home/app/(pages)/objects/page.js
+++ apps/🏂home/app/(pages)/objects/page.js
@@ -5,7 +5,14 @@ import Header from "@/components/header";
 export const metadata = {
   title: "Objects May Appear | NYSee",
   description:
-    "Gossamer is the original idea I&apos;d had for the first gallery.",
+    "The first gallery up on NYSee. Objects in mirror are closer than they appear",
+  url: "https://nysee.nyc/objects",
+  openGraph: {
+    url: "https://nysee.nyc/images/galleries/objects/og-objects.jpg",
+  },
+  robots: "index, follow",
+  keywords:
+    "Ray Weitzenberg, photographer, artist, technologist, new yorker, photography, design, risography, riso, NYSee, NYC, Sacto, Sacramento, ",
 };
 
 const GalleryObjects = () => {
@@ -14,22 +21,22 @@ const GalleryObjects = () => {
       <Header />
       <div className="gallery gallery--objects bg-white">
         <div className="hero">
-          <div className="w-5/6 max-w-screen-md mx-auto pt-24">
+          <div className="sm:w-5/6 max-w-screen-md mx-4 sm:mx-auto pt-8 sm:pt-24">
             <h1 className="text-3xl font-medium">Objects May Appear</h1>
             <h3 className="pt-4 text-1xl italic">
               Tuesday, 03 Sep 2024 14.01.50 PM EDT
             </h3>
             <p className="pt-4 text-xl">
               Objects May Appear was started in 2015 and ran through 2018. Many
-              summer evenings after work I&apos;d roam the city shooting just about
-              everything, but having a priority of finding these distortions in
-              car windows.
+              summer evenings after work I&apos;d roam the city shooting just
+              about everything, but having a priority of finding these
+              distortions in car windows.
             </p>
             <p className="pt-4 text-xl">
               Out of all the exposures I made, it was the one directly below,
-              the reflection in the Cadillac&apos;s hood, that struck me the most.
-              When Jessica Angel saw the image she said to me, &quot;This exploration
-              has reached the next level.&quot;
+              the reflection in the Cadillac&apos;s hood, that struck me the
+              most. When Jessica Angel saw the image she said to me, &quot;This
+              exploration has reached the next level.&quot;
             </p>
             <p className="pt-4 text-xl">
               That was the last pic I shot in the series.
@@ -37,21 +44,22 @@ const GalleryObjects = () => {
             <p className="pt-4 text-xl">
               Thinking about the feeling of finding and composing that last
               image, I realize now that one thing that hightened the experience
-              was finding this reflection in the Caddy&apos;s hood rather than as I
-              typically might in a car&apos;s windows. I think it was this departure
-              that was most striking to me.
+              was finding this reflection in the Caddy&apos;s hood rather than
+              as I typically might in a car&apos;s windows. I think it was this
+              departure that was most striking to me.
             </p>
             <p className="pt-4 text-xl">
-              Thinking further still, I&apos;m reminded of my excitement 2 weeks ago
-              when shooting the fractured reflections in the Newschool&apos;s
-              University Center windows. Discovering those stopped me and sucked
-              me into exploring them for not a short amount of time. Seems this
-              project could be reanimated in a new form.
+              Thinking further still, I&apos;m reminded of my excitement 2 weeks
+              ago when shooting the fractured reflections in the
+              Newschool&apos;s University Center windows. Discovering those
+              stopped me and sucked me into exploring them for not a short
+              amount of time. Seems this project could be reanimated in a new
+              form.
             </p>
           </div>
         </div>
         <div className="flex flex-col items-center w-full max-w-7xl mx-auto px-4">
-          <div className="w-full grid grid-cols-1 sm:grid-cols-2 justify-center gap-4 md:gap-8 mt-36">
+          <div className="w-full grid grid-cols-1 sm:grid-cols-2 justify-center gap-4 md:gap-8 mt-16 sm:mt-36">
             <Image
               src="/images/galleries/objects/deKadillac.jpg"
               width={629}
@@ -75,7 +83,7 @@ const GalleryObjects = () => {
               width={630}
               height={630}
             />
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-4 md:gap-8 mt-8 sm:mt-0">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-4 md:gap-8 sm:w-[60%] mt-8 sm:mt-0">
               <Image
                 className="self-center w-full h-auto object-contain"
                 alt="Crosby Wiper"
@@ -105,7 +113,7 @@ const GalleryObjects = () => {
               </div>
             </div>
             <Image
-              className="w-full sm:w-[calc(60%-2rem)] h-auto object-contain"
+              className="w-full sm:w-[60%] h-auto object-contain"
               alt="TITANPOINT (Concerned Citizen)"
               src="/images/galleries/objects/titanpoint.jpg"
               width={724}
--- apps/🏂home/app/(pages)/projects/page.js
+++ apps/🏂home/app/(pages)/projects/page.js
@@ -3,9 +3,14 @@ import Link from "next/link";
 import Header from "@/components/header";
 
 export const metadata = {
-  title: "Objects May Appear | NYSee",
-  description:
-    "Gossamer is the original idea I&apos;d had for the first gallery.",
+  title: "Projects | NYSee",
+  description: "The never static projects page.",
+  openGraph: {
+    url: "https://nysee.nyc/images/galleries/objects/og-juli-kosolapova.jpg",
+  },
+  robots: "index, follow",
+  keywords:
+    "Ray Weitzenberg, photographer, artist, technologist, new yorker, photography, design, risography, riso, NYSee, NYC, Sacto, Sacramento, ",
 };
 
 const GalleryObjects = () => {
@@ -14,12 +19,12 @@ const GalleryObjects = () => {
       <Header />
       <div className="gallery gallery--objects bg-white">
         <div className="hero">
-          <div className="w-5/6 max-w-screen-md mx-auto pt-24">
+          <div className="w-5/6 md:w-full max-w-screen-md mx-auto pt-24">
             <h1 className="text-3xl font-medium">Projects</h1>
           </div>
         </div>
         <Link href="/objects" className="">
-          <div className="w-5/6 max-w-screen-md mx-auto pt-10">
+          <div className="w-5/6 md:w-full max-w-screen-md mx-auto pt-10">
             <h2 className="text-2xl font-medium">Objects May Appear</h2>
             <h3 className="pt-4 text-1xl italic">
               Tuesday, 03 Sep 2024 14.48.14 PM EDT
@@ -28,16 +33,16 @@ const GalleryObjects = () => {
               Laid out the first gallery today. Beware, objects may appear.
             </p>
           </div>
-          <div className="flex flex-col items-center w-full max-w-3xl mx-auto pt-8 px-4">
-            <Image
-              src="/images/galleries/objects-may-full-page.jpg"
-              width={1080}
-              height={1080}
-              className="ww-5/6 max-w-screen-md h-auto sm:mb-32"
-              alt="Design with abstract eye on checkerboard background"
-            />
-          </div>
         </Link>
+        <div className="flex flex-col items-center w-full max-w-3xl mx-auto pt-8 px-4">
+          <Image
+            src="/images/galleries/objects-may-full-page.jpg"
+            width={1080}
+            height={1080}
+            className="ww-5/6 max-w-screen-md h-auto sm:mb-32"
+            alt="Design with abstract eye on checkerboard background"
+          />
+        </div>
       </div>
     </>
   );
