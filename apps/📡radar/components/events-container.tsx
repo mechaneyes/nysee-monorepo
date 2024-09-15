0b586e8fbdb3a1423526883c331fe5c8c5485f7f
mechaneyes <ray@mechaneyes.com>
2024-09-15 17:35:00 -0400

    type error on date-sorted.tsx ° stories styling
    ° negative margin + pos padding for stories nav on ios

--- apps/📡radar/components/events-container.tsx
+++ apps/📡radar/components/events-container.tsx
@@ -126,8 +126,8 @@ const EventsContainer: React.FC<{ allPosts: { edges: Post[] } }> = ({
   }, [morePosts, selectedDate]);
 
   return (
-    <div className="home__stories--container px-5 max-w-[1280px] mx-auto -mt-8 sm:mt-0">
-      <div id="stories" className="home__sub-nav pb-4">
+    <div className="home__stories--container px-5 max-w-[1280px] mx-auto -mt-24 sm:mt-0">
+      <div id="stories" className="home__sub-nav pt-8 pb-4">
         <EventDateList morePosts={morePosts} onDateSelect={setSelectedDate} />
       </div>
       {filteredPosts.length > 0 && <HomeStories posts={filteredPosts} />}
