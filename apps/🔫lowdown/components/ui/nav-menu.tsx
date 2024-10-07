67ba1aca7b93aaf92ddfc8b9f2a2b82be809022d
mechaneyes <ray@mechaneyes.com>
2024-10-07 11:24:04 -0400

    stripped out shadcn from lowdown nav
    ° massive ts errors that i couldn't fix
    ° component implementation wasn't necessary in the end

--- apps/🔫lowdown/components/ui/nav-menu.tsx
+++ apps/🔫lowdown/components/ui/nav-menu.tsx
@@ -1,37 +1,20 @@
 "use client";
 
-import * as React from "react";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
-
 export default function NavMenu() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList className="NavigationMenuList">
-        <NavigationMenuItem>
-          <a href="/" className="font-medium text-lg">
-            NYSee
-          </a>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <a href="/radar" className="pl-5 font-medium text-lg">
-            Radar
-          </a>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <a href="/ray-weitzenberg" className="pl-5 font-medium text-lg">
-            Ray
-          </a>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <a href="/lowdown/about" className="pl-5 font-medium text-lg">
-            About
-          </a>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="NavigationMenuList">
+      <a href="/" className="font-medium text-lg">
+        NYSee
+      </a>
+      <a href="/radar" className="pl-5 font-medium text-lg">
+        Radar
+      </a>
+      <a href="/ray-weitzenberg" className="pl-5 font-medium text-lg">
+        Ray
+      </a>
+      <a href="/lowdown/about" className="pl-5 font-medium text-lg">
+        About
+      </a>
+    </div>
   );
 }
