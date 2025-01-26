154d6eebb1b2d0b706ff292458332827bfa0ab83
Ray Weitzenberg <ray@mechaneyes.com>
2025-01-26 09:00:14 -0500

    emote for favicon on lowdown. radar previously

--- apps/🔫lowdown/pages/index.tsx
+++ apps/🔫lowdown/pages/index.tsx
@@ -33,6 +33,10 @@ export default function Index({ allPosts: { edges }, preview }) {
       <Head>
         <title>NYSee Lowdown</title>
         <meta property="og:title" content="NYSee Lowdown" key="title" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👁️</text></svg>"
+        />
         <meta property="og:url" content="https://nysee.nyc/lowdown" />
         <meta
           property="og:image"
