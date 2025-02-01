3c8cb7e9f2aa7d96b84733f0d7c206657787379f
Ray Weitzenberg <ray@mechaneyes.com>
2025-02-01 10:55:30 -0500

    werk: copy + alt text + theme & meta colors

--- apps/🏂home/app/(pages)/werk/layout.tsx
+++ apps/🏂home/app/(pages)/werk/layout.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 export const metadata = {
   title: "Werk | Ray Weitzenberg | NYSee",
   description: "'Always be Creating' —Glengarray Glen Ross",
@@ -15,6 +17,27 @@ export const metadata = {
     "Ray Weitzenberg, photographer, artist, technologist, new yorker, photography, design, risography, riso, NYSee, NYC, Sacto, Sacramento, ",
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="bg-namjunepaik-1-light min-h-screen">{children}</div>;
+export const viewport = {
+  themeColor: "rgb(15 23 42 / 0.9)",
+};
+
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </Head>
+      <body className="bg-namjunepaik-1-light">
+        <div className="bg-namjunepaik-1-light min-h-screen">{children}</div>
+      </body>
+    </html>
+  );
 }
+
+// export default function Layout({ children }: { children: React.ReactNode }) {
+//   return <div className="bg-namjunepaik-1-light min-h-screen">{children}</div>;
+// }
--- apps/🏂home/app/(pages)/werk/page.js
+++ apps/🏂home/app/(pages)/werk/page.js
@@ -8,7 +8,7 @@ const Werk = () => {
       <div className="bg-slate-900/90">
         <Header />
       </div>
-      <main className="min-h-screen p-4 md:p-8 md:pt-12 mx-auto pb-12 lg:pb-24 space-y-6">
+      <main className="min-h-screen p-4 pt-7 md:p-8 md:pt-12 mx-auto pb-12 lg:pb-24 space-y-6">
         <header className="w-full max-w-[1200px] mx-auto">
           <h1 className="text-5xl text-namjunepaik-3 font-[cholla-unicase]">
             Ray Weitzenberg
@@ -19,14 +19,14 @@ const Werk = () => {
 
           <div className="w-full lg:w-2/3 pt-2 text-xl font-light text-namjunepaik-5">
             <p className="pt-1 leading-relaxed">
-              Below are screenshots and brief descriptions of some of my
-              recent projects.
+              Below are screenshots and brief descriptions of some of my recent
+              projects.
             </p>
             <p className="leading-relaxed">
-              Let&apos;s create something together:{" "}
+              Let&apos;s create together:{" "}
               <Link
                 href="/ray-weitzenberg"
-                className="text-namjunepaik-3 no-underline hover:text-[#ff8c26] hover:no-underline transition-colors duration-300"
+                className="text-namjunepaik-3 no-underline hover:text-[#ff8c26] hover:no-underline transition-colors duration-300 whitespace-nowrap"
               >
                 nysee.nyc/ray-weitzenberg
               </Link>
@@ -59,33 +59,37 @@ const Werk = () => {
           <div className="w-full pt-4">
             <Image
               src="/images/act/scopus-ex-machina.png"
-              alt="Dead Dinner"
+              alt="Cover image of Scopus Ex Machina Presentation Deck"
               width={1200}
               height={743}
             />
             <Image
               src="/images/act/scopus-2.png"
-              alt="Dead Dinner"
+              alt="Scopus Ex Machina Slide. Text explaining considerations of surveillance."
               width={1200}
               height={743}
+              className="pt-1 lg:pt-0"
             />
             <Image
               src="/images/act/scopus-3.png"
-              alt="Dead Dinner"
+              alt="Scopus Ex Machina Slide. Section title slide. Text stating 'Watching the watchers.' Illustration of an eyeball looking at that text."
               width={1200}
               height={743}
+              className="pt-1 lg:pt-0"
             />
             <Image
               src="/images/act/scopus-4a.png"
-              alt="Dead Dinner"
+              alt="Scopus Ex Machina Slide. Title states, 'Scopeo: Roll your own facial recognition.' Picture of a camera looking out a window."
               width={1200}
               height={743}
+              className="pt-1 lg:pt-0"
             />
             <Image
               src="/images/act/scopus-5.png"
-              alt="Dead Dinner"
+              alt="Scopus Ex Machina Slide. Title states, 'Just because you're paranoid doesn't mean they're not out to get you.' Picture of a cat wearing a tinfoil hat."
               width={1200}
               height={743}
+              className="pt-1 lg:pt-0"
             />
           </div>
         </div>
@@ -112,25 +116,25 @@ const Werk = () => {
           <div className="w-full grid grid-cols-2 md:grid-cols-4 justify-start gap-4 pt-8">
             <Image
               src="/images/act/dd-mob-1.png"
-              alt="Dead Dinner"
+              alt="Dead Dinner Mobile Screenshot. Cover image of Malcolm X."
               width={535}
               height={1062}
             />
             <Image
               src="/images/act/dd-mob-2.png"
-              alt="Dead Dinner"
+              alt="Dead Dinner Mobile Screenshot. Nation of Islam page."
               width={535}
               height={1062}
             />
             <Image
               src="/images/act/dd-mob-3.png"
-              alt="Dead Dinner"
+              alt="Dead Dinner Mobile Screenshot. Prison Period page."
               width={535}
               height={1062}
             />
             <Image
               src="/images/act/dd-mob-4.png"
-              alt="Dead Dinner"
+              alt="Dead Dinner Mobile Screenshot. Birth and Early Life page."
               width={535}
               height={1062}
             />
@@ -138,21 +142,23 @@ const Werk = () => {
           <div className="w-full pt-8">
             <Image
               src="/images/act/dd-1.png"
-              alt="Dead Dinner"
+              alt="Dead Dinner Desktop  Screenshot. Birth and Early Life page."
               width={1200}
               height={743}
             />
             <Image
               src="/images/act/dd-2.png"
-              alt="Dead Dinner"
+              alt="Dead Dinner Mobile Screenshot. Nation of Islam page."
               width={1200}
               height={743}
+              className="pt-1 lg:pt-0"
             />
             <Image
               src="/images/act/dd-3.png"
-              alt="Dead Dinner"
+              alt="Dead Dinner Mobile Screenshot. Pilgrimage to Mecca page."
               width={1200}
               height={743}
+              className="pt-1 lg:pt-0"
             />
           </div>
         </div>
@@ -174,19 +180,19 @@ const Werk = () => {
           <div className="w-full pt-4">
             <Image
               src="/images/act/thirdeyes-1.png"
-              alt="Dead Dinner"
+              alt="Thirdeyes Desktop Screenshot."
               width={1200}
               height={743}
             />
             <Image
               src="/images/act/thirdeyes-2.png"
-              alt="Dead Dinner"
+              alt="Thirdeyes Desktop Screenshot."
               width={1200}
               height={743}
             />
             <Image
               src="/images/act/thirdeyes-3.png"
-              alt="Dead Dinner"
+              alt="Thirdeyes Desktop Screenshot."
               width={1200}
               height={743}
             />
@@ -205,50 +211,52 @@ const Werk = () => {
                 href="/radar"
                 className="pt-2 text-namjunepaik-3 no-underline hover:text-[#ff8c26] hover:no-underline transition-colors duration-300"
               >
-                nysee.nyc/radar
-              </Link> <br />
+                /radar
+              </Link>{" "}
+              <br />
               <Link
                 href="/lowdown"
                 className="pt-2 text-namjunepaik-3 no-underline hover:text-[#ff8c26] hover:no-underline transition-colors duration-300"
               >
-                nysee.nyc/lowdown
-              </Link> <br />
+                /lowdown
+              </Link>{" "}
+              <br />
               <Link
                 href="/lowdown/about"
                 className="pt-2 text-namjunepaik-3 no-underline hover:text-[#ff8c26] hover:no-underline transition-colors duration-300"
               >
-                nysee.nyc/lowdown/about
+                /about
               </Link>
             </p>
           </div>
           <div className="w-full pt-4">
             <Image
               src="/images/act/nysee-nyc.jpg"
-              alt="Dead Dinner"
+              alt="NYSee Desktop Screenshot. Homepage."
               width={1200}
               height={743}
               className="drop-shadow-2xl"
             />
             <Image
               src="/images/act/nysee-radar.jpg"
-              alt="Dead Dinner"
+              alt="NYSee Radar Desktop Screenshot. Text left. Background image of a landscape. Radar antenna in the bottom right."
               width={1200}
               height={743}
-              className="pt-8 lg:pt-14 drop-shadow-2xl"
+              className="pt-6 lg:pt-14 drop-shadow-2xl"
             />
             <Image
               src="/images/act/nysee-radar-scroll.jpg"
-              alt="Dead Dinner"
+              alt="NYSee Radar Desktop Screenshot. Text left. Background image of a landscape. Radar antenna in the bottom right."
               width={1200}
               height={743}
-              className="pt-8 lg:pt-14 drop-shadow-2xl"
+              className="pt-6 lg:pt-14 drop-shadow-2xl"
             />
             <Image
               src="/images/act/nysee-lowdown.jpg"
-              alt="Dead Dinner"
+              alt="NYSee Lowdown Desktop Screenshot. Masthead up top. Abstract image filling the page."
               width={1200}
               height={743}
-              className="pt-8 lg:pt-14 drop-shadow-2xl"
+              className="pt-6 lg:pt-14 drop-shadow-2xl"
             />
           </div>
         </div>
@@ -272,7 +280,7 @@ const Werk = () => {
           <div className="w-full pt-4">
             <Image
               src="/images/act/mechaneyes-1.jpg"
-              alt="Dead Dinner"
+              alt="Mechaneyes Desktop Screenshot. Topographic map illustration."
               width={1920}
               height={1080}
               quality={100}
@@ -280,27 +288,27 @@ const Werk = () => {
             />
             <Image
               src="/images/act/mechaneyes-2.jpg"
-              alt="Dead Dinner"
+              alt="Mechaneyes Desktop Screenshot. Abstract image with floral pattern."
               width={1280}
               height={712}
               quality={100}
-              className="pt-8 lg:pt-14 drop-shadow-2xl"
+              className="pt-6 lg:pt-14 drop-shadow-2xl"
             />
             <Image
               src="/images/act/mechaneyes-3.jpg"
-              alt="Dead Dinner"
+              alt="Mechaneyes Desktop Screenshot. Topographic map illustration. Map points demarking mountain peaks."
               width={1280}
               height={712}
               quality={100}
-              className="pt-8 lg:pt-14 drop-shadow-2xl"
+              className="pt-6 lg:pt-14 drop-shadow-2xl"
             />
             <Image
               src="/images/act/mechaneyes-4.jpg"
-              alt="Dead Dinner"
+              alt="Mechaneyes Desktop Screenshot. Banner image looking down on the Hudson River to the NYC cityscape."
               width={1280}
               height={712}
               quality={100}
-              className="pt-8 lg:pt-14 drop-shadow-2xl"
+              className="pt-6 lg:pt-14 drop-shadow-2xl"
             />
           </div>
         </div>
@@ -323,6 +331,7 @@ const Werk = () => {
           <div className="w-full pt-4">
             <Image
               src="/images/no-ordinary-duotone-1.0.0.jpg"
+              alt="Risograph print. Woman on a foggy dance floor lit from behind with a strobe light."
               width={1920}
               height={1435}
               className="w-full lg:w-4/5 h-auto"
@@ -348,10 +357,10 @@ const Werk = () => {
           </div>
           <Image
             src="/images/galleries/objects-may-full-page.jpg"
+            alt="Screenshot of Objects May Appear gallery. Images arranged in a grid."
             width={1024}
             height={2199}
             className="w-auto h-full max-h-[90vh] pt-4 drop-shadow-2xl"
-            alt="Design with abstract eye on checkerboard background"
           />
         </div>
       </main>
